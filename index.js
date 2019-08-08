e7f9f2d8676d5c6cdca621ccdc9fdb2b93b6e17b
Brian Douglas <bdougie@users.noreply.github.com>
2019-08-07 17:16:19 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Octokit = require('@octokit/rest')
 const octokit = new Octokit({
-  auth: process.env.GITHUB_TOKEN, // 
+  auth: process.env.GITHUB_TOKEN, // https://github.com/settings/tokens/new
   previews: ['application/vnd.github.baptiste-preview+json']
 })
 
@@ -10,7 +10,7 @@ const OWNER = "_github_username_"
 octokit.repos.listForUser({username: OWNER, per_page: 100, page: 1}).then((res) => {
   res.data.forEach((x) => {
     octokit.repos.update({
-      owner: OWNER"fakeAssBrian",
+      owner: OWNER,
       repo: x.name,
       archived: true
       }).then((res) => console.log("SUCCESS: ", res))

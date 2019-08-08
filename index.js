a616f5752b436bcdcf1f8f1d1ad219b7bd8b7103
Brian Douglas <bdougie@users.noreply.github.com>
2019-08-07 17:07:33 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+const Octokit = require('@octokit/rest')
+const octokit = new Octokit({
+  auth: process.env.GITHUB_TOKEN, // 
+  previews: ['application/vnd.github.baptiste-preview+json']
+})
+
+const OWNER = "_github_username_"
+
+// Archive all repos
+octokit.repos.listForUser({username: OWNER, per_page: 100, page: 1}).then((res) => {
+  res.data.forEach((x) => {
+    octokit.repos.update({
+      owner: OWNER"fakeAssBrian",
+      repo: x.name,
+      archived: true
+      }).then((res) => console.log("SUCCESS: ", res))
+    .catch((err) => console.log("ERROR: ", err))
+  })
+}).catch((err) => console.log("LIST ERROR: ", err))
